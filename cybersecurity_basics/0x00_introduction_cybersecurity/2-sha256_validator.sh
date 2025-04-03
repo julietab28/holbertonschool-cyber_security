f6fa2faf64865531b011c384b37c54fcbdbd1c5b
julietabobadilla <julietabobadilla123@gmail.com>
2025-04-03 20:11:41 -0300

    Task 2 - sha256 Validator(fixed)

--- cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "$2 $1" > hashed.txt || sha256sum -c hashed.txt && echo test_file: OK || echo test_file: FAILED
+echo "$2 $1" > hashed.txt | sha256sum -c hashed.txt && echo test_file: OK || echo test_file: FAILED
